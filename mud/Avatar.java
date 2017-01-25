f13dcf33eee04277ca6bacc640705eb43d674bf1
Christoffer <christofferj93@gmail.com>
2017-01-25 22:53:50 +0100

    asd

---
+++ mud/Avatar.java
@@ -0,0 +1,9 @@
+public class Avatar {
+
+    private int HP = 60; //värde mellan 0-180, kolla exception? kanske inte ens initiera här
+    private list finishedCourses = ["Bervet", "PKD", "Automatateori", "Intro-IT", "MDI", "Linjär Algebra I"];
+    private list unfinishedCourses = [];
+    private int backpackCapacity = 10;
+    private list backpack = [];
+    
+}
